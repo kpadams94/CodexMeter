a4fe45346c3c973a2dba788f5f0f739f2a2270d2
Kyle Adams <kpadams100@gmail.com>
2026-08-01 12:52:37 -0400

    Suppress reset notification popups

--- src/CodexMeter/WindowsNotificationSink.cs
+++ src/CodexMeter/WindowsNotificationSink.cs
@@ -14,6 +14,6 @@ public sealed class WindowsNotificationSink : INotificationSink
             {
                 Silent = true,
             })
-            .Show();
+            .Show(toast => toast.SuppressPopup = true);
     }
 }
